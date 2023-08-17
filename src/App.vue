954dfd00449f5a1b7e5f67ace5aef388bc575094
Gaurav Rastogi <124706980+Gaurav-coding08@users.noreply.github.com>
2023-08-17 12:13:11 +0400

    Api file

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <router-link to="/Services">Services</router-link> |
+    <router-link to="/Registration">Registration</router-link> |
+    <router-link to="/Unregister">Unregister</router-link>
+  </nav>-->
+  <router-view/> 
 </template>
 
 <style lang="scss">
